e1ec8e201fdc78096a8fac8bf669a793549fed60
kareemhossam-0 <kareemelden0@gmail.com>
2025-04-14 20:18:00 +0200

    finish hadith tab

---
+++ lib/presentation/main_layout/tabs/hadith/hadith.dart
@@ -0,0 +1,45 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../config/theme/assets_manager.dart';
+import 'widgets/hadith_card.dart';
+
+class Hadith extends StatelessWidget {
+  const Hadith({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(
+                AssetsManager.hadithTabBackgroundImage,
+              ))),
+      child: Column(
+        children: [
+          Expanded(flex: 1, child: Image.asset(AssetsManager.islamiLogo)),
+          Expanded(
+            flex: 4,
+            child: CarouselSlider(
+                options: CarouselOptions(
+                  height: 600,
+                  viewportFraction: 0.7,
+                  enlargeCenterPage: true,
+                ),
+                items: List.generate(
+                  50,
+                  (index) => index + 1,
+                )
+                    .map(
+                      (index) => HadithCard(index: index),
+                    )
+                    .toList()),
+          )
+        ],
+      ),
+    );
+  }
+}
